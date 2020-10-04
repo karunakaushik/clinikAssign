69c6b7dcda845e7928a433e444117e6ea3989e2a
karuna14 <kkaushik@bt.iitr.ac.in>
2020-10-05 03:08:18 +0530

    Updates

---
+++ precache-manifest.b6d9d6d727c08180c0687ccb47ca7f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65460b97d66b5844cf3cd3c253609c4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f787083740b0754beca1",
+    "url": "/static/css/main.5fcbfdd8.chunk.css"
+  },
+  {
+    "revision": "7df95fdd6a785daf1f3c",
+    "url": "/static/js/2.07733047.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07733047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f787083740b0754beca1",
+    "url": "/static/js/main.c4c2d0f4.chunk.js"
+  },
+  {
+    "revision": "8f24bbadc26d3d7964e8",
+    "url": "/static/js/runtime-main.c24485ae.js"
+  },
+  {
+    "revision": "511a849aec256dcfc2ad423e304855d4",
+    "url": "/static/media/graph.511a849a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clinikAssign/precache-manifest.4e31f1828bc55d9240afb7a3db4d2d70.js"
+  "/precache-manifest.b6d9d6d727c08180c0687ccb47ca7f5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clinikAssign/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
