8ad715885347c3b60fe02d0d5e17aee25c8a3087
karuna14 <kkaushik@bt.iitr.ac.in>
2020-10-05 02:59:41 +0530

    Updates

---
+++ precache-manifest.cf322ebb729d4c121b1bedccd81d29b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3021a31eb0b5653ddfbc43bcefc168",
+    "url": "/clinikAssign/index.html"
+  },
+  {
+    "revision": "b61cdcd689f88e16f6d1",
+    "url": "/clinikAssign/static/css/main.5fcbfdd8.chunk.css"
+  },
+  {
+    "revision": "7df95fdd6a785daf1f3c",
+    "url": "/clinikAssign/static/js/2.07733047.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clinikAssign/static/js/2.07733047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61cdcd689f88e16f6d1",
+    "url": "/clinikAssign/static/js/main.8334ccd8.chunk.js"
+  },
+  {
+    "revision": "be456f3952b2e883e83e",
+    "url": "/clinikAssign/static/js/runtime-main.5d7ce42f.js"
+  },
+  {
+    "revision": "511a849aec256dcfc2ad423e304855d4",
+    "url": "/clinikAssign/static/media/graph.511a849a.png"
+  }
+]);
